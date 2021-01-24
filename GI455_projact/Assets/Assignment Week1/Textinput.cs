5195b9b59ff000bad7e22551641e8a2ef7a5d03f
DESKTOP-7PMPVU1\Windows10 Pro <peerapon.leaw@bumail.net>
2021-01-24 23:52:24 +0700

    Send Assignment Week1

---
+++ GI455_projact/Assets/Assignment Week1/Textinput.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Textinput : MonoBehaviour
+{
+    public string[] data = new string[5];
+    public InputField inputword;
+    private string myData;
+    public Text showText;
+    private void Start()
+    {
+       data[0] = "Wood";
+       data[1] = "Fly";
+       data[2] = "Over";
+       data[3] = "Try";
+       data[4] = "Hello";
+
+    }
+    void Update()
+    {
+        myData = inputword.text;
+      
+    }
+    public void ButtonFind()
+    {
+        if(data[0] == myData)
+        {
+            isfound();
+        }
+        else if(data[1]== myData)
+        {
+            isfound();
+        }
+        else if(data[2] == myData)
+        {
+            isfound();
+        }
+        else if (data[3] == myData)
+        {
+            isfound();
+        }
+        else if (data[4] == myData)
+        {
+            isfound();
+        }
+        else
+        {
+            showText.text = " [ " + $"<color=red>{myData}</color>" + " ] " + " is not found. ";
+        }
+
+
+    }   
+    
+    void isfound()
+    {
+        showText.text = " [ " + $"<color=green>{myData}</color>" + " ] " + " is found. ";
+    }   
+   
+
+
+
+
+
+}
